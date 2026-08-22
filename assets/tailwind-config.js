b73320a78ef30696a63cc362f13180e3fda28864
Farzad <ojwinservice@gmail.com>
2026-08-22 23:47:44 +0330

    Add files via upload

---
+++ assets/tailwind-config.js
@@ -0,0 +1,154 @@
+tailwind.config = {
+          darkMode: "class",
+          theme: {
+            extend: {
+              "colors": {
+                      "inverse-surface": "#2f3132",
+                      "tertiary-container": "#001e3c",
+                      "on-background": "#1a1c1d",
+                      "background": "#f9f9fb",
+                      "surface-container": "#eeeef0",
+                      "on-tertiary-fixed": "#001c39",
+                      "on-secondary-container": "#002d61",
+                      "surface-container-low": "#f3f3f5",
+                      "error": "#ba1a1a",
+                      "on-secondary-fixed-variant": "#00458e",
+                      "primary-fixed": "#e4e2e4",
+                      "on-surface": "#1a1c1d",
+                      "surface-container-highest": "#e2e2e4",
+                      "outline-variant": "#c7c6ca",
+                      "tertiary": "#00030b",
+                      "surface-tint": "#5f5e60",
+                      "on-primary": "#ffffff",
+                      "secondary-fixed-dim": "#aac7ff",
+                      "primary-fixed-dim": "#c8c6c8",
+                      "surface": "#f9f9fb",
+                      "inverse-primary": "#c8c6c8",
+                      "on-error": "#ffffff",
+                      "surface-container-high": "#e8e8ea",
+                      "on-secondary-fixed": "#001b3e",
+                      "on-secondary": "#ffffff",
+                      "on-tertiary-container": "#0087ed",
+                      "on-primary-fixed": "#1b1b1d",
+                      "surface-variant": "#e2e2e4",
+                      "tertiary-fixed-dim": "#a3c9ff",
+                      "on-tertiary": "#ffffff",
+                      "on-primary-container": "#868587",
+                      "secondary": "#005cba",
+                      "on-primary-fixed-variant": "#474649",
+                      "tertiary-fixed": "#d3e3ff",
+                      "secondary-container": "#5095fe",
+                      "error-container": "#ffdad6",
+                      "inverse-on-surface": "#f0f0f2",
+                      "on-error-container": "#93000a",
+                      "primary": "#030304",
+                      "outline": "#77767b",
+                      "surface-container-lowest": "#ffffff",
+                      "on-surface-variant": "#46464a",
+                      "surface-bright": "#f9f9fb",
+                      "primary-container": "#1d1d1f",
+                      "surface-dim": "#d9dadc",
+                      "secondary-fixed": "#d7e3ff",
+                      "on-tertiary-fixed-variant": "#004882"
+              },
+              "borderRadius": {
+                      "DEFAULT": "0.25rem",
+                      "lg": "0.5rem",
+                      "xl": "0.75rem",
+                      "full": "9999px"
+              },
+              "spacing": {
+                      "section-gap-desktop": "120px",
+                      "margin-desktop": "auto",
+                      "section-gap-mobile": "64px",
+                      "max-width": "1200px",
+                      "gutter": "24px"
+              },
+              "fontFamily": {
+                      "headline-lg": [
+                              "Vazirmatn"
+                      ],
+                      "body-lg": [
+                              "Vazirmatn"
+                      ],
+                      "label-md": [
+                              "Vazirmatn"
+                      ],
+                      "display-lg": [
+                              "Vazirmatn"
+                      ],
+                      "caption": [
+                              "Vazirmatn"
+                      ],
+                      "body-md": [
+                              "Vazirmatn"
+                      ],
+                      "display-lg-mobile": [
+                              "Vazirmatn"
+                      ],
+                      "headline-xl": [
+                              "Vazirmatn"
+                      ]
+              },
+              "fontSize": {
+                      "headline-lg": [
+                              "32px",
+                              {
+                                      "lineHeight": "1.25",
+                                      "fontWeight": "700"
+                              }
+                      ],
+                      "body-lg": [
+                              "21px",
+                              {
+                                      "lineHeight": "1.5",
+                                      "fontWeight": "400"
+                              }
+                      ],
+                      "label-md": [
+                              "14px",
+                              {
+                                      "lineHeight": "1.3",
+                                      "fontWeight": "500"
+                              }
+                      ],
+                      "display-lg": [
+                              "56px",
+                              {
+                                      "lineHeight": "1.1",
+                                      "letterSpacing": "-0.02em",
+                                      "fontWeight": "800"
+                              }
+                      ],
+                      "caption": [
+                              "12px",
+                              {
+                                      "lineHeight": "1.2",
+                                      "fontWeight": "400"
+                              }
+                      ],
+                      "body-md": [
+                              "17px",
+                              {
+                                      "lineHeight": "1.47",
+                                      "fontWeight": "400"
+                              }
+                      ],
+                      "display-lg-mobile": [
+                              "40px",
+                              {
+                                      "lineHeight": "1.2",
+                                      "fontWeight": "800"
+                              }
+                      ],
+                      "headline-xl": [
+                              "40px",
+                              {
+                                      "lineHeight": "1.2",
+                                      "fontWeight": "700"
+                              }
+                      ]
+              }
+            }
+          }
+        }
